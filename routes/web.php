a49d436441da1c0c42e8eb20d649bff390f578f0
rerain <1rerain1@gmail.com>
2026-01-20 15:28:35 +0100

    deploy.6

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,11 @@ Route::get('/schedule', GroupExamScheduleExplorer::class)
     ->name('public.schedule')->withoutMiddleware(['auth'])
     ->name('public.schedule');
 ;
+
+Route::get('/setup-db', function () {
+    Artisan::call('session:table');
+    Artisan::call('cache:table');
+    Artisan::call('migrate', ['--force' => true]);
+    Artisan::call('db:seed', ['--class' => 'RealisticUniversitySeeder']);
+    return "✅ Database setup complete!";
+});
