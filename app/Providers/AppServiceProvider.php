62546d2911b3617e893877db870bd216ead90ec0
rerain <1rerain1@gmail.com>
2026-01-21 21:37:12 +0100

    d

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
         Gate::policy(Department::class, DepartmentPolicy::class);
         Gate::policy(\App\Models\Student::class, \App\Policies\StudentPolicy::class);
 
-
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
